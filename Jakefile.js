7e35c1a3e7c6c0c49381f72eb9b634b3bc8a9d0d
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2017-06-21 17:04:57 +0900

    fixes #2

---
+++ Jakefile.js
@@ -0,0 +1,25 @@
+var jake = require('jake');
+var exec = require('child_process').exec;
+var shellescape = require('shell-escape');
+var config = require('config').config;
+
+jake.task('start', function() {
+  var command = [
+    'pm2',
+    'start',
+    'app.js',
+    '--name=' + shellescape([config.application.name])
+  ].join(' ');
+  exec(command);
+  console.log('start');
+});
+
+jake.task('stop', function() {
+  var command = [
+    'pm2',
+    'stop',
+    shellescape([config.application.name])
+  ].join(' ');
+  exec(command);
+  console.log('stop');
+});
