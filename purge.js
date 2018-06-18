cdf411e52278132d4625ce7ce26f45e778d04cdb
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2018-06-18 11:32:11 +0900

    #38

--- purge.js
+++ purge.js
@@ -2,22 +2,36 @@
 const config = require('config').config;
 const fs = require('fs');
 const path = require('path');
+const posix = require('posix');
 
+const dir = path.join(__dirname, 'uploads');
 const yesterday = new Date();
 yesterday.setDate(yesterday.getDate() - config.purge.days);
 
-fs.readdir(path.join(__dirname, 'uploads'), (error, files) => {
-  files.filter((file) => {
-    const fileStat = fs.statSync(path.join(__dirname, 'uploads', file));
-    return fileStat.isFile() && !file.match(/^\./) && (fileStat.mtime < yesterday);
-  }).forEach((file) => {
-    const filePath = path.join(__dirname, 'uploads', file);
-    fs.unlink(filePath, (error) => {
-      if (error) {
-        console.error('%j', {script:'purge', path:filePath, message:error});
-      } else {
-        console.info('%j', {script:'purge', path:filePath, message:'deleted'});
-      }
+const log = (message, priority = 'info') => {
+  posix.openlog('picon', {ndelay: true, pid: true}, 'user');
+  posix.syslog(priority, JSON.stringify(message));
+  posix.closelog();
+};
+
+fs.readdir(dir, (error, files) => {
+  Promise.resolve().then(() => {
+    log({script:'purge', dir:dir, message:'start'});
+  }).then(() => {
+    files.filter(file => {
+      const fileStat = fs.statSync(path.join(__dirname, 'uploads', file));
+      return fileStat.isFile() && !file.match(/^\./) && (fileStat.mtime < yesterday);
+    }).forEach(file => {
+      const filePath = path.join(__dirname, 'uploads', file);
+      fs.unlink(filePath, error => {
+        if (error) {
+          log({script:'purge', path:filePath, message:error}, 'crit');
+        } else {
+          log({script:'purge', path:filePath, message:'deleted'});
+        }
+      });
     });
+  }).then(() => {
+    log({script:'purge', dir:dir, message:'end'});
   });
 });
