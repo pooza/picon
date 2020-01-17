b78906558fff7877bd5aa6d7e048a3f546c1b777
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-01-17 21:34:05 +0900

    .

--- picon.js
+++ picon.js
@@ -1,119 +1,119 @@
-'use strict';
+'use strict'
 
-const config = require('config').config;
-const fs = require('fs');
-const crypto = require('crypto');
-const path = require('path');
-const exec = require('child_process').exec;
-const shellescape = require('shell-escape');
-const filetype = require('file-type');
-const {CronJob} = require('cron');
-const gm = require('gm').subClass({imageMagick: true});
-const ffmpeg = require('fluent-ffmpeg');
-const app = require('express')();
-const upload = require('multer')({dest: path.join(__dirname, 'tmp')});
+const config = require('config').config
+const fs = require('fs')
+const crypto = require('crypto')
+const path = require('path')
+const exec = require('child_process').exec
+const shellescape = require('shell-escape')
+const filetype = require('file-type')
+const {CronJob} = require('cron')
+const gm = require('gm').subClass({imageMagick: true})
+const ffmpeg = require('fluent-ffmpeg')
+const app = require('express')()
+const upload = require('multer')({dest: path.join(__dirname, 'tmp')})
 
-app.listen(config.server.port, config.server.address);
+app.listen(config.server.port, config.server.address)
 config.package = JSON.parse(
   fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8')
-);
-const message = {request: {}, response: {}};
+)
+const message = {request: {}, response: {}}
 console.info('%j', {
   message: 'starting...',
   package: {name: config.package.name, version: config.package.version},
   server: {port: config.server.port},
-});
+})
 
 const createFileName = (f, params) => {
-  const values = [];
+  const values = []
   Object.keys(params).forEach(k => {
-    values.push(k);
-    values.push(params[k]);
-  });
-  const sha1 = crypto.createHash('sha1');
-  values.push(fs.readFileSync(f));
-  sha1.update(values.join('::'));
-  return sha1.digest('hex') + '.png';
-};
+    values.push(k)
+    values.push(params[k])
+  })
+  const sha1 = crypto.createHash('sha1')
+  values.push(fs.readFileSync(f))
+  sha1.update(values.join('::'))
+  return sha1.digest('hex') + '.png'
+}
 
 const sendImage = (response, f, params) => {
   fs.readFile(f, (error, contents) => {
     if (error) {
-      throw new Error(error);
+      throw new Error(error)
     } else {
-      console.info('%j', message);
-      response.header('Content-Type', 'image/png');
-      response.end(contents);
+      console.info('%j', message)
+      response.header('Content-Type', 'image/png')
+      response.end(contents)
     }
   })
-};
+}
 
 const getType = f => {
-  return filetype(fs.readFileSync(f)).mime;
-};
+  return filetype(fs.readFileSync(f)).mime
+}
 
 const isExist = f => {
   try {
-    fs.statSync(f);
+    fs.statSync(f)
     return true
   } catch (error) {
     return false
   }
-};
+}
 
 const isPDF = f => {
-  return getType(f) == 'application/pdf';
-};
+  return getType(f) == 'application/pdf'
+}
 
 const isVideo = f => {
-  return config.video.types.indexOf(getType(f)) != -1;
-};
+  return config.video.types.indexOf(getType(f)) != -1
+}
 
 const isOfficeDocument = f => {
-  return config.office.types.indexOf(getType(f)) != -1;
-};
+  return config.office.types.indexOf(getType(f)) != -1
+}
 
 const convertPDF = f => {
   return new Promise((resolve, reject) => {
-    const dest = path.join(__dirname, 'tmp', path.basename(f, '.png') + '.png');
+    const dest = path.join(__dirname, 'tmp', path.basename(f, '.png') + '.png')
     gm(f).write(dest, error => {
       if (error) {
-        message.error = {status: 400, message: error};
-        throw new Error(message.error.message);
+        message.error = {status: 400, message: error}
+        throw new Error(message.error.message)
       }
       [
         dest,
         path.join(path.dirname(dest), path.basename(dest, '.png') + '-0.png'),
       ].forEach(name => {
         if (isExist(name)) {
-          console.info('%j', {action: 'convert', type: 'pdf', src: f, dest: name});
-          resolve(name);
+          console.info('%j', {action: 'convert', type: 'pdf', src: f, dest: name})
+          resolve(name)
         }
       })
-    });
-  });
-};
+    })
+  })
+}
 
 const convertVideo = f => {
   return new Promise((resolve, reject) => {
-    const dest = path.join(__dirname, 'tmp', path.basename(f) + '.png');
+    const dest = path.join(__dirname, 'tmp', path.basename(f) + '.png')
     ffmpeg(f).screenshots({
       timemarks: [0],
       folder: path.dirname(dest),
       filename: path.basename(dest),
     }).on('end', (stdout, stderr) => {
-      console.info('%j', {action: 'convert', type: 'video', src: f, dest: dest});
-      resolve(dest);
+      console.info('%j', {action: 'convert', type: 'video', src: f, dest: dest})
+      resolve(dest)
     }).on('error', (error, stdout, stderr) => {
-      message.error = {status: 400, message: error};
-      throw new Error(message.error.message);
-    });
-  });
-};
+      message.error = {status: 400, message: error}
+      throw new Error(message.error.message)
+    })
+  })
+}
 
 const convertOfficeDocument = f => {
   return new Promise((resolve, reject) => {
-    const dest = path.join(__dirname, 'tmp', path.basename(f) + '.png');
+    const dest = path.join(__dirname, 'tmp', path.basename(f) + '.png')
     const command = [
       'libreoffice',
       '--headless',
@@ -122,72 +122,72 @@ const convertOfficeDocument = f => {
       '--convert-to', 'png',
       '--outdir', shellescape([path.dirname(dest)]),
       shellescape([f]),
-    ].join(' ');
+    ].join(' ')
     exec(command, (error, stdout, stderr) => {
       if (error) {
-        message.error = {status: 400, message: error};
-        throw new Error(message.error.message);
+        message.error = {status: 400, message: error}
+        throw new Error(message.error.message)
       } else {
-        console.info('%j', {action: 'convert', type: 'office', src: f, dest: dest});
-        resolve(dest);
+        console.info('%j', {action: 'convert', type: 'office', src: f, dest: dest})
+        resolve(dest)
       }
-    });
-  });
-};
+    })
+  })
+}
 
 app.get('/about', (request, response, next) => {
-  message.request = {path: request.path};
-  message.response = {};
-  delete message.error;
-  console.info('%j', message);
+  message.request = {path: request.path}
+  message.response = {}
+  delete message.error
+  console.info('%j', message)
   response.json({
     package: config.package,
     config: config.server,
     purge: config.purge,
-  });
-});
+  })
+})
 
 app.post('/convert', upload.single('file'), (request, response, next) => {
-  const params = Object.assign({}, request.body);
-  params.function = 'convert';
-  message.request = {path: request.path};
-  delete message.error;
+  const params = Object.assign({}, request.body)
+  params.function = 'convert'
+  message.request = {path: request.path}
+  delete message.error
 
   if (!request.file) {
-    message.error = {status: 400, message: 'File was not posted'};
-    throw new Error(message.error.message);
+    message.error = {status: 400, message: 'File was not posted'}
+    throw new Error(message.error.message)
   } else if (isPDF(request.file.path)) {
     convertPDF(request.file.path).then(dest => {
-      sendImage(response, dest, params);
-    });
+      sendImage(response, dest, params)
+    })
   } else if (isVideo(request.file.path)) {
     convertVideo(request.file.path).then(dest => {
-      sendImage(response, dest, params);
-    });
+      sendImage(response, dest, params)
+    })
   } else if (isOfficeDocument(request.file.path)) {
     convertOfficeDocument(request.file.path).then(dest => {
-      sendImage(response, dest, params);
-    });
+      sendImage(response, dest, params)
+    })
   } else {
-    message.error = {status: 400, message: 'Invalid file type'};
-    throw new Error(message.error.message);
+    message.error = {status: 400, message: 'Invalid file type'}
+    throw new Error(message.error.message)
   }
-});
+})
 
 app.post('/resize', upload.single('file'), (request, response, next) => {
   if (!request.file) {
-    message.error = {status: 400, message: 'File was not posted'};
-    throw new Error(message.error.message);
+    message.error = {status: 400, message: 'File was not posted'}
+    throw new Error(message.error.message)
   }
 
-  const params = Object.assign({}, request.body);
-  params.function = 'resize';
-  params.width = (params.width || 100);
-  params.height = (params.height || 100);
-  params.background_color = (params.background_color || 'white');
-  message.request = {params: params, path: request.path};
-  const dest = path.join(__dirname, 'tmp', createFileName(request.file.path, params));
-  delete message.error;
+  const params = Object.assign({}, request.body)
+  params.function = 'resize'
+  params.width = (params.width || 100)
+  params.height = (params.height || 100)
+  params.background_color = (params.background_color || 'white')
+  message.request = {params: params, path: request.path}
+  const dest = path.join(__dirname, 'tmp', createFileName(request.file.path, params))
+  delete message.error
 
   gm(request.file.path)
     .resize(params.width, params.height)
@@ -196,75 +196,75 @@ app.post('/resize', upload.single('file'), (request, response, next) => {
     .extent(params.width, params.height)
     .write(dest, error => {
       if (error) {
-        message.error = {status: 400, message: error};
-        throw new Error(message.error.message);
+        message.error = {status: 400, message: error}
+        throw new Error(message.error.message)
       } else {
-        message.response = {action: 'resize', sent: dest};
-        sendImage(response, dest, params);
+        message.response = {action: 'resize', sent: dest}
+        sendImage(response, dest, params)
       }
-    });
-});
+    })
+})
 
 app.post('/resize_width', upload.single('file'), (request, response, next) => {
   if (!request.file) {
-    message.error = {status: 400, message: 'File was not posted'};
-    throw new Error(message.error.message);
+    message.error = {status: 400, message: 'File was not posted'}
+    throw new Error(message.error.message)
   }
 
-  const params = Object.assign({}, request.body);
-  params.function = 'resize_width';
-  params.width = (params.width || 100);
-  params.method = (params.method || 'resize');
-  message.request = {params: params, path: request.path};
-  const dest = path.join(__dirname, 'tmp', createFileName(request.file.path, params));
-  delete message.error;
+  const params = Object.assign({}, request.body)
+  params.function = 'resize_width'
+  params.width = (params.width || 100)
+  params.method = (params.method || 'resize')
+  message.request = {params: params, path: request.path}
+  const dest = path.join(__dirname, 'tmp', createFileName(request.file.path, params))
+  delete message.error
 
   gm(request.file.path)[params.method](params.width, null).write(dest, error => {
     if (error) {
-      message.error = {status: 400, message: error};
-      throw new Error(message.error.message);
+      message.error = {status: 400, message: error}
+      throw new Error(message.error.message)
     } else {
-      message.response = {action: 'resize', sent: dest};
-      sendImage(response, dest, params);
+      message.response = {action: 'resize', sent: dest}
+      sendImage(response, dest, params)
     }
-  });
-});
+  })
+})
 
 app.use((request, response, next) => {
-  message.request = {params: request.query, path: request.path};
-  message.response = {};
-  message.error = {status: 404, message: 'Not found'};
-  console.error('%j', message);
-  response.status(message.error.status);
-  response.json(message);
-});
+  message.request = {params: request.query, path: request.path}
+  message.response = {}
+  message.error = {status: 404, message: 'Not found'}
+  console.error('%j', message)
+  response.status(message.error.status)
+  response.json(message)
+})
 
 app.use((error, request, response, next) => {
-  message.request = {params: request.query, path: request.path};
-  message.response = {};
-  message.error = message.error || {status: 500, message: error.message};
-  console.error('%j', message);
-  response.status(message.error.status);
-  response.json(message);
-});
+  message.request = {params: request.query, path: request.path}
+  message.response = {}
+  message.error = message.error || {status: 500, message: error.message}
+  console.error('%j', message)
+  response.status(message.error.status)
+  response.json(message)
+})
 
 new CronJob(config.purge.cron, () => {
-  const dir = path.join(__dirname, 'tmp');
+  const dir = path.join(__dirname, 'tmp')
   fs.readdir(dir, (error, files) => {
-    const yesterday = new Date();
-    yesterday.setDate(yesterday.getDate() - config.purge.days);
+    const yesterday = new Date()
+    yesterday.setDate(yesterday.getDate() - config.purge.days)
 
     files.filter(f => {
-      const stat = fs.statSync(path.join(__dirname, 'tmp', f));
-      return stat.isFile() && !f.match(/^\./) && (stat.mtime < yesterday);
+      const stat = fs.statSync(path.join(__dirname, 'tmp', f))
+      return stat.isFile() && !f.match(/^\./) && (stat.mtime < yesterday)
     }).forEach(f => {
       fs.unlink(path.join(dir, f), error => {
         if (error) {
-          console.error('%j', {path: path.join(dir, f), error: error});
+          console.error('%j', {path: path.join(dir, f), error: error})
         } else {
-          console.info('%j', {path: path.join(dir, f), action: 'delete'});
+          console.info('%j', {path: path.join(dir, f), action: 'delete'})
         }
-      });
-    });
-  });
-}, null, true);
+      })
+    })
+  })
+}, null, true)
---
+++ test.js
@@ -0,0 +1,60 @@
+'use strict'
+
+const request = require('supertest')
+const path = require('path')
+const config = require('config').config
+const app = request('http://localhost:' + config.server.port)
+
+app.get('/notfound')
+  .expect(404)
+  .expect('Content-Type', /json/)
+  .catch(err => {throw err})
+
+app.get('/about')
+  .expect(200)
+  .expect('Content-Type', /json/)
+  .catch(err => {throw err})
+
+const dir = path.join(__dirname, 'sample')
+
+let exts = ['jpg', 'png', 'gif', 'tiff', 'webp']
+let requestPaths = ['/resize', '/resize_width']
+
+requestPaths.forEach(requestPath => {
+  app.post(requestPath)
+    .expect(400)
+    .expect('Content-Type', /json/)
+    .then(e => {console.info(requestPath + ' (empty) OK')})
+    .catch(e => {throw e})
+
+  exts.forEach(ext => {
+    const filePath = path.join(dir, 'sample.' + ext)
+    app.post(requestPath)
+      .attach('file', filePath)
+      .expect(200)
+      .expect('Content-Type', /png/)
+      .then(e => {console.info(requestPath + ' ' + ext + ' OK')})
+      .catch(e => {throw e})
+  });
+});
+
+exts = ['flv', 'mp4', 'pdf']
+requestPaths = ['/convert']
+
+requestPaths.forEach(requestPath => {
+  app.post(requestPath)
+    .expect(400)
+    .expect('Content-Type', /json/)
+    .then(e => {console.info(requestPath + ' (empty) OK')})
+    .catch(e => {throw e})
+
+  exts.forEach(ext => {
+    const filePath = path.join(dir, 'sample.' + ext)
+    app.post(requestPath)
+      .attach('file', filePath)
+      .expect(200)
+      .expect('Content-Type', /png/)
+      .then(e => {console.info(requestPath + ' ' + ext + ' OK')})
+      .catch(e => {throw e})
+  });
+});
