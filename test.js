9bebafd1d8fc1de36f6b67d451456efaa759d52b
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-01-19 09:23:56 +0900

    #65

--- test.js
+++ test.js
@@ -4,14 +4,25 @@ const request = require('supertest')
 const path = require('path')
 const config = require('config').config
 const app = request('http://localhost:' + config.server.port)
+const entries = [
+  {
+    requestPaths: ['/resize', '/resize_width'],
+    exts: ['jpg', 'png', 'gif', 'bmp', 'tiff', 'webp'],
+  },
+  {
+    requestPaths: ['/convert'],
+    exts: ['pdf', 'flv', 'mp4'],
+  },
+]
 
 app.get('/notfound')
   .expect(404)
   .expect('Content-Type', /json/)
   .then(e => {
-    console.info('/notfound OK')
+    console.info('OK /notfound')
   }).catch(e => {
     console.error(e.message)
+    console.info('NG /notfound')
     process.exit(1)
   })
 
@@ -19,72 +30,39 @@ app.get('/about')
   .expect(200)
   .expect('Content-Type', /json/)
   .then(e => {
-    console.info('/about OK')
+    console.info('OK /about')
   }).catch(e => {
     console.error(e.message)
+    console.info('NG /about')
     process.exit(1)
   })
 
-const dir = path.join(__dirname, 'sample')
-
-let exts = ['jpg', 'png', 'gif', 'tiff', 'webp']
-let requestPaths = ['/resize', '/resize_width']
-
-requestPaths.forEach(requestPath => {
-  app.post(requestPath)
-    .expect(400)
-    .expect('Content-Type', /json/)
-    .then(e => {
-      console.info(requestPath + ' (empty) OK')
-    }).catch(e => {
-      console.error(e.message)
-      console.error(requestPath + ' (empty) NG')
-      process.exit(1)
-    })
-
-  exts.forEach(ext => {
-    const filePath = path.join(dir, 'sample.' + ext)
+entries.forEach(entry => {
+  entry.requestPaths.forEach(requestPath => {
     app.post(requestPath)
-      .attach('file', filePath)
-      .expect(200)
-      .expect('Content-Type', /png/)
+      .expect(400)
+      .expect('Content-Type', /json/)
       .then(e => {
-        console.info(requestPath + ' ' + ext + ' OK')
+        console.info('OK (empty) ' + requestPath)
       }).catch(e => {
         console.error(e.message)
-        console.error(requestPath + ' ' + ext + ' NG')
+        console.error('NG (empty) ' + requestPath)
         process.exit(1)
       })
+
+    entry.exts.forEach(ext => {
+      const filePath = path.join(__dirname, 'sample/sample.' + ext)
+      app.post(requestPath)
+        .attach('file', filePath)
+        .expect(200)
+        .expect('Content-Type', /png/)
+        .then(e => {
+          console.info('OK .' + ext + ' ' + requestPath)
+        }).catch(e => {
+          console.error(e.message)
+          console.info('NG .' + ext + ' ' + requestPath)
+          process.exit(1)
+        })
+    });
   });
-});
-
-exts = ['pdf', 'flv', 'mp4']
-requestPaths = ['/convert']
-
-requestPaths.forEach(requestPath => {
-  app.post(requestPath)
-    .expect(400)
-    .expect('Content-Type', /json/)
-    .then(e => {
-      console.info(requestPath + ' (empty) OK')
-    }).catch(e => {
-      console.error(e.message)
-      console.error(requestPath + ' (empty) NG')
-      process.exit(1)
-    })
-
-  exts.forEach(ext => {
-    const filePath = path.join(dir, 'sample.' + ext)
-    app.post(requestPath)
-      .attach('file', filePath)
-      .expect(200)
-      .expect('Content-Type', /png/)
-      .then(e => {
-        console.info(requestPath + ' ' + ext + ' OK')
-      }).catch(e => {
-        console.error(e.message)
-        console.error(requestPath + ' ' + ext + ' NG')
-        process.exit(1)
-      })
-  });
-});
+})
